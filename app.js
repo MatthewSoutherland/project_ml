f1124f9f3652c6e751c7d98e3796cc5366617b45
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2026-01-18 14:53:31 -0500

    feat: added listener to list item 1

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 
 document.addEventListener("DOMContentLoaded", async function() {
     console.log("js read");
+    const listItem1 = document.getElementById("item-1");
+    listItem1.addEventListener("click", function() {
+        alert("list item 1 clicked");
+    });
 });
